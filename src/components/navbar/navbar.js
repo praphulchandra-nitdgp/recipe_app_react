8f3d22f0655288a5617719415305c9e53616e69c
praphulchandra-nitdgp <156814647+praphulchandra-nitdgp@users.noreply.github.com>
2024-06-02 10:20:31 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUtensils, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils, faMagnifyingGlass, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css';
 
 const Navbar = ({ updateSearch, getSearch, search }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header>
       <div className="navbar">
+
+        <button className={`navbar-menu-btn ${isMenuOpen ? 'hidden' : ''}`} id="menu-btn" onClick={toggleMenu} >
+          <i className='fa-bars'><FontAwesomeIcon icon={faBars}/></i>
+        </button>
+        <button className={`navbar-menu-close-btn ${isMenuOpen ? '' : 'hidden'}`} id="close-btn" onClick={toggleMenu} style={{ display: isMenuOpen ? 'block' : 'none' }}>
+          <i className='fa-times'><FontAwesomeIcon icon={faTimes}/></i>
+        </button>
+
         <a href="#"><h1 className="navbar-brand"><FontAwesomeIcon icon={faUtensils}/> Recipe</h1></a>
         <nav>
-          <ul className="navbar-nav">
+          <ul className={`navbar-nav ${isMenuOpen ? 'active' : ''}`}>
             <li><a href="#" className="navbar-link">Home</a></li>
             <li><a href="#about-us" className="navbar-link">About Us</a></li>
           </ul>
